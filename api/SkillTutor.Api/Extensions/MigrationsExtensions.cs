f40d0c6c61093675a858666acb7902be02741537
dedicdino <112410825+dedicdino@users.noreply.github.com>
2025-08-07 11:24:06 +0200

    added asp identity user

---
+++ api/SkillTutor.Api/Extensions/MigrationsExtensions.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using SkillTutor.Database;
+
+namespace SkillTutor.Api.Extensions;
+
+public static class MigrationsExtensions
+{
+    public static void ApplyMigrations(this IApplicationBuilder app)
+    {
+        using IServiceScope scope = app.ApplicationServices.CreateScope();
+        using SkillTutorDbContext context = scope.ServiceProvider.GetRequiredService<SkillTutorDbContext>();
+        context.Database.Migrate();
+    }
+}
--- api/SkillTutor.Api/Program.cs
+++ api/SkillTutor.Api/Program.cs
@@ -1,41 +1,31 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SkillTutor.Api.Extensions;
+using SkillTutor.Database;
+using SkillTutor.Models.Core;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication()
+    .AddBearerToken(IdentityConstants.BearerScheme);
+builder.Services.AddIdentityCore<User>()
+    .AddEntityFrameworkStores<SkillTutorDbContext>()
+    .AddApiEndpoints();
+builder.Services.AddDbContext<SkillTutorDbContext>(options => 
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.ApplyMigrations();
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
